e09eb52b10ccee4a8e7eec4981f15f174745859c
Daniel Suo <danielsuo@gmail.com>
2017-08-23 01:32:28 -0500

    Get socket.io working

--- display.py
+++ display.py
@@ -1,9 +1,14 @@
 import os
 import random
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__,
             template_folder=os.path.abspath('src/templates'))
+app.config['SECRET_KEY'] = 'crazy_secret'
+
+socketio = SocketIO(app, async_mode=None)
 
 
 @app.route('/')
@@ -12,5 +17,20 @@ def index():
     return render_template('index.html', random_seed=random.randint(1, 9999))
 
 
+@socketio.on('connect')
+def test_message():
+    print('Client connected')
+
+
+@socketio.on('event')
+def test_event(message):
+    print('Event!')
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+io = require('socket.io-client');
 components = require('./components');
 
 const element = <components.Dummy dummy='ruh roh'/>;
@@ -5,3 +6,17 @@ ReactDOM.render(
   element,
   document.getElementById('root')
 );
+
+var url = 'http://' + document.domain + ':' + location.port
+console.log(url);
+var socket = io.connect(url);
+
+socket.on('connect', function() {
+  console.log('connected!')
+  socket.emit('event', {data: 'connected!'});
+
+});
+socket.on('response', function(msg) {
+  console.log('hello!')
+  console.log(msg.data);
+});
